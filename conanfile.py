5c6f5ec1aaf3a26b451855ec781f8020c17656ec
Ulrich Eck <ulrich.eck@tum.de>
2019-07-16 12:07:29 +0200

    adapted to camposs

--- conanfile.py
+++ conanfile.py
@@ -25,7 +25,7 @@ class LibExpatConan(ConanFile):
         del self.settings.compiler.libcxx
 
     def requirements(self):
-        self.requires("common/1.0.0@sight/stable")
+        self.requires("ircad_common/1.0.0@camposs/stable")
 
     def source(self):
         tools.get("https://github.com/libexpat/libexpat/archive/R_{0}.tar.gz".format(self.upstream_version.replace(".", "_")))
